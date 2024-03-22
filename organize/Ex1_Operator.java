07d121958e7ade5e97ccc3326c06d3c9a8efa3b8
zzooong2 <zzooong2@naver.com>
2024-03-22 11:35:39 +0900

    5일차 수업

---
+++ organize/Ex1_Operator.java
@@ -0,0 +1,68 @@
+package kr.co.organize;
+
+public class Ex1_Operator {
+	
+	/*
+	
+		 < 연산자 >
+		 1. 산술 연산자 (+ - / * %)
+			int a = 10;
+			int b = 20;
+		
+		    a + b = 30;
+		    a - b = -10;
+		    a / b = 0;
+		    a * b = 200;
+		    a % b = 10;
+		 
+		 2. 증감 연산자 (++ --)
+			int a;
+			
+		    a++; -> 1증가
+		    a--; -> 1감소
+		   
+		 3. 비교 연산자 (> < >= <= == !=)
+		 	a > b -> a가 b보다 크다 (초과)
+		 	a < b -> a가 b보다 작다 (미만)
+		 	a >= b -> a가 b보다 크거나 같다 (이상)
+		 	a <= b -> a가 b보다 작거나 같다 (이하)
+		 	a == b -> a와 b는 같다
+		 	a != b -> a와 b는 같지않다
+		 
+		 4. 논리 부정 연산자 (!)
+		 	- 연산결과가 true 일 경우 false 반환
+		 	- 연산결과가 false 일 경우 true 반환
+		 
+		 	boolean isTrue = true;
+		 	System.out.println(!isTrue); // 논리 부정 연산자로 인해 false 반환
+		 	
+		 	String s1 = "apple";
+		 	String s2 = new String("apple");
+		 	
+		 	// 데이터(객체) 비교
+		 	System.out.println(s1 == s2); // 0x100 == 0x200 
+		 	
+		 	// 문자열이 같으면 true, 다르면 false
+		 	System.out.println(s1.equals(s2));
+		 	
+		 	// 문자열이 같으면 false, 다르면 ture
+		 	System.out.println(!s1.equals(s2));
+		 	
+		 
+		 5. 논리 연산자 (&& ||)
+		 	- && : 연산결과 모두 참일 때 참 반환
+		 	- || : 연산결과 하나라도 참일 때 참 반환 (왼쪽 연산이 참일경우, 오른쪽 연산 진행 X)
+		 	
+		 	
+		 6. 복합 대입 연산자 (+= -= /= *= %=)
+		 	- 산술 연산자와 대입 연산자를 하나로 나타낼 수 있는 연산
+		 	a += 5; -> a = a + 5;
+		 	b -= 5; -> b = b - 5;
+		 	
+		 
+		 7. 삼항 연산자 (조건 ? 참 : 거짓)
+		 	- 가독성이 떨어지므로 사용을 지양함
+		 	int result = a>b ? a+10 : b-5; 
+	 */
+	
+}
---
+++ organize/Ex4_Array.java
@@ -0,0 +1,21 @@
+package kr.co.organize;
+
+public class Ex4_Array {
+
+	/*
+		
+		< 배열 >
+		- 같은 자료형을 가진 변수를 하나로 묶어서 관리
+		
+		[인덱스]
+		- 배열 안의 각 요소에 접근하기 위해 사용되는 번호
+		- 각 요소의 주소값을 대신한다고 생각하면 편함
+		- 0부터 시작
+		- 존재하지 않는 인덱스에 접근하면 Out of Bounds Exception 발생
+		
+		int[] array = new int[10]; // 배열 크기 = 10, 인덱스 = 0~9
+		
+			
+	*/
+	
+}
