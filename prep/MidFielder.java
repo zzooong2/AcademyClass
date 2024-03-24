c5d02933f2d333cf2a45194934198bea5814b33e
zzooong2 <zzooong2@naver.com>
2024-03-24 19:49:30 +0900

    Java 상속 공부

---
+++ prep/MidFielder.java
@@ -0,0 +1,26 @@
+package kr.co.prep;
+
+public class MidFielder extends Player {
+	
+	String role;
+	int roleNumber;
+	
+	public MidFielder(String name, String position, int backNumber, String role, int roleNumber) {
+		this.name = name;
+		this.position = position;
+		this.backNumber = backNumber;
+		
+		this.role = role;
+		this.roleNumber = roleNumber;
+		
+	}
+	
+	public void midFielderRole() {
+		playerInfo();
+		
+		System.out.println("선수 역할: " + role);
+		System.out.println("부여된 롤: " + roleNumber + "번");
+
+	}
+
+}
