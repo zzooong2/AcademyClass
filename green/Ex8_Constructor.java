e99ca0628128e5e812018880ab6afb3f48dba7a1
zzooong2 <zzooong2@naver.com>
2024-03-27 12:08:33 +0900

    8일차 수업

--- green/Ex8_Constructor.java
+++ green/Ex8_Constructor.java
@@ -6,6 +6,15 @@ public class Ex8_Constructor {
 		
 		Ex8_1_Calculator cal = new Ex8_1_Calculator(40, 31, "김현중");
 		
+		
+		Ex8_2_Student student = new Ex8_2_Student();
+		
+		System.out.println(student.getName());
+		
+		student.setName("김현중");
+		
+		System.out.println(student.getName());
+		
 	}
 
 }
--- quiz/Ex8_Constructor.java
+++ quiz/Ex8_Constructor.java
@@ -28,10 +28,37 @@ public class Ex8_Constructor {
 		
 		buy.buyMouse();
 		
-		buy.buyComputer();
+		buy.buyComputer();		
 		
+		System.out.println();		
+		System.out.println("---------------------------------------");
 		
+		// Ex8_3_Student
+		Ex8_3_Student student = new Ex8_3_Student();
 		
-	}
+		System.out.println("---------------------------------------");
+		
+		// Ex8_4_Order
+		// 기본생성자 X -> Getter, Setter 이용
+		// 주문 번호(orderNumber) ->  ORD123
+		// 주문 금액(orderAmount) ->  100.5
+		// 주문 상태(orderStatus) ->  처리중
+		Ex8_4_Order order = new Ex8_4_Order("ORD123", 100.5, "처리중" );
+		
+		System.out.println(order.getOrderNumber());
+		System.out.println(order.getOrderAmount());
+		System.out.println(order.getOrderStatus());
+		
+		order.setOrderAmount(80);
+		
+		System.out.println("---------------------------------------");
+		
+		// 메소드 생성 및 출력
+		// 주문금액 100 미만이면 "유효하지 않은 금액입니다" 출력
+		// 주문금액 100 이상이면 주문번호, 금액, 상태 출력
+		order.amountCheck();
+	}	
+	
+	
 
 }
