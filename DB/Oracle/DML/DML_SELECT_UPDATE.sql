0bdf0309a242fb45613f759b358f730f5c9bc701
zzooong2 <zzooong2@naver.com>
2024-05-01 14:55:48 +0900

    DML-SELECT 수정 공부

---
+++ DB/Oracle/DML/DML_SELECT_UPDATE.sql
@@ -0,0 +1,38 @@
+--UPDATE: 테이블의 기존 데이터를 변경하는 구문
+
+SELECT * FROM EMPLOYEES e
+
+UPDATE
+	EMPLOYEES
+SET
+	FIRST_NAME = '현중'
+WHERE
+	EMPLOYEE_ID = 213;
+	
+
+--------------------------------------------------------------------------------
+
+--1. EMPLOYEES 테이블의 Donald 직원 데이터를 아래와 같이 변경하세요.
+--EMAIL: Donald, PHONE_NUMBER: 01044445555, COMMISSION_PCT: 1.5
+SELECT * FROM EMPLOYEES e 
+WHERE PHONE_NUMBER = '590.423.4567'
+WHERE FIRST_NAME = 'Donald'
+
+UPDATE
+	EMPLOYEES
+SET
+	EMAIL = 'Donald',
+	PHONE_NUMBER = 01044445555,
+	COMMISSION_PCT = '0.5'
+WHERE
+	EMPLOYEE_ID = 198;
+
+--2. EMPLOYEES 테이블에 590.423.4567 핸드폰을 가진 직원을 아래와 같이 변경하세요.
+--HIRE_DATE: 현재 날짜, SALARY: 15,000
+UPDATE
+	EMPLOYEES
+SET
+	HIRE_DATE = sysdate,
+	SALARY = 15000
+WHERE
+	PHONE_NUMBER = '590.423.4567';
