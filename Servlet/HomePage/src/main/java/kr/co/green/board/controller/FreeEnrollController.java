5959de47457fd01d203f0ab3f83c4dc73ea23f5a
zzooong2 <zzooong2@naver.com>
2024-05-20 17:36:10 +0900

    FreeEnrollController 작성

--- Servlet/HomePage/src/main/java/kr/co/green/board/controller/FreeEnrollController.java
+++ Servlet/HomePage/src/main/java/kr/co/green/board/controller/FreeEnrollController.java
@@ -1,6 +1,8 @@
 package kr.co.green.board.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import kr.co.green.board.model.dto.FreeDTOImpl;
 import kr.co.green.board.model.service.FreeServiceImpl;
@@ -44,17 +47,54 @@ public class FreeEnrollController extends HttpServlet {
 		freeDTO.setBoardContent(content);
 		freeDTO.setMemberNo(memberNo);
 		
+		// 파일 업로드
+		Collection<Part> parts = request.getParts();
+		String uploadDirectory = "/Users/zzooong2/green/Green/src/kr/co/Servlet/HomePage/src/main/webapp/resources/uploads/freeBoard";
+		
+		// 파일 업로드 디렉토리가 존재하지 않으면 생성
+		File filePath = new File(uploadDirectory);
+		if(!filePath.exists()) {
+			filePath.mkdirs();
+		}
+
 		// 서비스 호출
 		FreeServiceImpl freeService = new FreeServiceImpl();
+		int result = freeService.enroll(freeDTO); // 게시글 등록
 		
-		int result = freeService.enroll(freeDTO);
+		// 게시글 번호 가져오기
+		freeService.selectNo(freeDTO);
+		
+		
+		// 게시글에 파일 첨부하기
+		String fileName = null;
+		for(Part part : parts) {
+			fileName = getFileName(part);
+			if(fileName != null) {
+				part.write(filePath + File.separator + fileName);
+				
+				freeDTO.setFilePath(uploadDirectory);
+				freeDTO.setFileName(fileName);
+				int resultUpload = freeService.fileUpload(freeDTO);
+			}
+		}
 		
 		if(result == 1) {
-			response.sendRedirect("/freeBoard/list.do?cPage=1");
+			response.sendRedirect("/freeBoard/list.do?cPage=1&category=fb_title&seach-text=");
 		} else {
 			RequestDispatcher view = request.getRequestDispatcher("/views/board/free/freeEnroll.jsp");
 			view.forward(request, response);
 		}
 	}
+
+    private String getFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        String[] tokens = contentDisposition.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf('=') + 2, token.length() - 1);
+            }
+        }
+        return null;
+    }
 
 }
